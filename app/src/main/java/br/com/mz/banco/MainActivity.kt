4914d875ecbd0a1791d907b786174e9e97f5ac54
MZaqueu <30877800+marcuszaqueu@users.noreply.github.com>
2021-07-01 11:26:04 -0300

    Initial commit

---
+++ app/src/main/java/br/com/mz/banco/MainActivity.kt
@@ -0,0 +1,30 @@
+package br.com.mz.banco
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import br.com.mz.banco.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.iSaldo.setOnClickListener{
+            val intent = Intent(this,Saldo::class.java)
+            startActivity(intent)
+        }
+
+        binding.tSaldo.setOnClickListener{
+            val intent = Intent(this,Saldo::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
---
+++ app/src/main/java/br/com/mz/banco/Saldo.kt
@@ -0,0 +1,11 @@
+package br.com.mz.banco
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Saldo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saldo)
+    }
+}
